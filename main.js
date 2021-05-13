0bc033cf6ea9e07c16b6544eabe2b78589436b75
jay kirit maru <80564737+JayMaru20@users.noreply.github.com>
2021-05-13 19:00:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function creation(elem, parent, content = "", attributes = []){
     let element = document.createElement(elem);
     attributes.forEach((value) => {
-        element.setAttribute(value[0],value[1]);
+        element.setAttribute(value[0], value[1]);
     });
     element.textContent = content;
     parent.append(element);
@@ -11,50 +11,42 @@ function creation(elem, parent, content = "", attributes = []){
 fetch('https://restcountries.eu/rest/v2/all')
     .then((resp) => {
     return resp.json();
-    }).then((data) => {
-        let container = creation('div',document.body,"",[['class','container pt-5']]);
-        let row = creation('div',container,"",['class','row']);
-        Country(data,row);
+    }).then((resp) => {
+        let container = creation('div',document.body,"",[['class', 'container pt-5']]);
+        let row = creation('div',container,"",[['class', 'row'],['style','margin:10px;']]);
+        Country(resp,row);
     }).catch((error) => {
     console.log(error);
-    });
+});
 
-function Country(data,row){
-    data.forEach((value) => {
-        let column = creation('div',row,"",[
-            ['class','col-lg-4 card'],
-            ["style", "background:skyblue; margin-collapse:collapse; margin-radius:0%; margin:5px;"]]);
+function Country(resp,row){
+    resp.forEach((value) => {
+        let col = creation('div',row,'',[['class','col-lg-4 card p-3'],['style', 'background:skyblue;']]);
 
-        let cardHeader = creation('div',column,value.name,[
-            ['class','card-header'],
-        ["style","color:white; text-align:center; background:black;"]]);
+        let header = creation('div',col , value.name ,[['class','card-header'],['style','color:white; text-align:center; background:black;']]);
 
-        let cardBody = creation('div',column,"",[
-            ['class','card-body'],
-            ['style','text-align:center; color:white; background:linear-gradient(to right,black,gray);']]);
+        let mainBody = creation('div',col,'',[['class','card-body'],['style','text-align:center; color:white; background:linear-gradient(to right,black,gray);']]);
 
-        let flag = creation ('img',cardBody,"",[
-            ['src',value.flag],
-            ['class','card-img-top']]);
+        let flag = creation ('img',mainBody,'',[['src',value.flag],['class','card-img-top']]);
 
         let text1 = 'Capital:'+value.capital;
         let text2 = 'Region:'+ value.region;
         let text3 = 'Country Code:'+value.alpha3Code;
        
-        let bodyText1 = creation("div",cardBody,text1,[['class','card-text']]);
-        let bodyText2 = creation("div",cardBody,text2,[['class','card-text']]);
-        let bodyText3 = creation("div",cardBody,text3,[['class','card-text']]);
+        let bodyText1 = creation("p",mainBody,text1,[['class','card-text']]);
+        let bodyText2 = creation("p",mainBody,text2,[['class','card-text']]);
+        let bodyText3 = creation("p",mainBody,text3,[['class','card-text']]);
 
-        let button = creation('button',cardBody,"Click for Weather",[['class','btn btn-primary']]);
+        let button = creation('button',mainBody,"Click for Weather",[['class','btn btn-primary']]);
         button.onclick = () => {
             setTimeout(() => {
-                weather(value);
+                weather(value, mainBody);
             },2000);
         };
     });
 }
 
-function weather(value)
+function weather(value, mainBody)
 {
     let latitude = value.latlng[0];
     let longitude = value.latlng[1];
@@ -62,8 +54,8 @@ function weather(value)
     fetch(url)
     .then((resp) => {
         return resp.json();
-    }).then((data) => {
-        let text1 = data.main.temp;
+    }).then((tempdata) => {
+        let text1 = tempdata.main.temp;
         alert("Temperature = " +parseFloat(text1-273.15)+'celcius');
     }).catch((error) => {
         console.log("error");
